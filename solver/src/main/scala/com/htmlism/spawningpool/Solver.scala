d01df2ec6fde32a77d82074ee3cdd64e94855482
Mark Canlas <github@htmlism.com>
2014-07-08 02:43:34 -0400

    added empty seed check

--- solver/src/main/scala/com/htmlism/spawningpool/Solver.scala
+++ solver/src/main/scala/com/htmlism/spawningpool/Solver.scala
@@ -1,5 +1,9 @@
 package com.htmlism.spawningpool
 
 class Solver {
-  def solve[T](seed: Traversable[T]): Set[T] = seed.toSet
+  def solve[T](seed: Traversable[T]): Set[T] =
+    if (seed.isEmpty)
+      throw new IllegalArgumentException("must provide a non-empty collection as a seed")
+    else
+      seed.toSet
 }
