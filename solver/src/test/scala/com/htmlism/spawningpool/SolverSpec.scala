9ca48cd238a61d5e22b3509e511bcdc25c8ff893
Mark Canlas <github@htmlism.com>
2014-07-08 14:04:58 -0400

    gutted main

--- solver/src/test/scala/com/htmlism/spawningpool/SolverSpec.scala
+++ solver/src/test/scala/com/htmlism/spawningpool/SolverSpec.scala
@@ -3,4 +3,20 @@ package com.htmlism.spawningpool
 import org.specs2.mutable.Specification
 
 class SolverSpec extends Specification {
+  "Given an initial population seed, the solver" should {
+    "return the right solution" in {
+      val solver = new Solver
+
+      solver.solve(Seq('chrono, 'marle, 'lucca)) === Set('chrono, 'marle, 'lucca)
+    }
+  }
+
+  "Given no initial population seed, the solver" should {
+    "return solutions" in {
+      val solver = new Solver(3)
+      implicit val generator = new ChromosomeIterator('cindy, 'sanda, 'mindy)
+
+      solver.solve === Set('cindy, 'sanda, 'mindy)
+    }
+  }
 }
