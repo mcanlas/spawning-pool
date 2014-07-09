736ba1a2b35958d4d7b8122cc75974ed3a421159
Mark Canlas <github@htmlism.com>
2014-07-09 12:50:27 -0400

    added population guard

--- solver/src/main/scala/com/htmlism/spawningpool/Solver.scala
+++ solver/src/main/scala/com/htmlism/spawningpool/Solver.scala
@@ -5,6 +5,9 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 
 class Solver(populationSize: Int = 50)(implicit rig: RandomIndexGenerator) {
+  if (populationSize < 1)
+    throw new IllegalArgumentException("must have a population size of one or greater")
+
   def solve[T](implicit src: Generator[T]): Future[Set[T]] = solve(Traversable.fill(populationSize)(src.generate))
 
   def solve[T](seed: Traversable[T]): Future[Set[T]] =
