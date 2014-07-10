26f408a7a8fe2491900872c93324537d9934c4cb
Mark Canlas <github@htmlism.com>
2014-07-10 17:21:16 -0400

    created chromosome classes

--- solver/src/main/scala/com/htmlism/spawningpool/FixedLengthChromosome.scala
+++ solver/src/main/scala/com/htmlism/spawningpool/FixedLengthChromosome.scala
@@ -1,3 +1,6 @@
 package com.htmlism.spawningpool
 
-trait FixedLengthChromosome[A] extends IndexedChromosome[A]
+trait FixedLengthChromosome[A] extends IndexedChromosome[A] {
+  // an optimization given that both chromosomes are the same length
+  override def crossover(mate: Chromosome) = ??? // TODO implement fixed ceiling crossover given rig
+}
--- solver/src/main/scala/com/htmlism/spawningpool/VariableLengthChromosome.scala
+++ solver/src/main/scala/com/htmlism/spawningpool/VariableLengthChromosome.scala
@@ -1,3 +1,9 @@
 package com.htmlism.spawningpool
 
-trait VariableLengthChromosome[A] extends IndexedChromosome[A]
+trait VariableLengthChromosome[A] extends IndexedChromosome[A] {
+  override def mutate = {
+    // TODO given rig, choose add in n + 1, delete in n, or spot mutate
+
+    ???
+  }
+}
