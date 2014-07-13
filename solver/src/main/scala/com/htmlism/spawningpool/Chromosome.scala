653bf0fc1160883ea2944476d28b0b3acff44b27
Mark Canlas <github@htmlism.com>
2014-07-12 23:19:19 -0400

    expanded base doc

--- solver/src/main/scala/com/htmlism/spawningpool/Chromosome.scala
+++ solver/src/main/scala/com/htmlism/spawningpool/Chromosome.scala
@@ -1,9 +1,29 @@
 package com.htmlism.spawningpool
 
-/** The base trait for chromosomes that support mutation and crossover. */
+/** The base trait for chromosomes that support mutation and crossover.
+  *
+  * {{{
+  *   def mutate: Chromosome
+  *
+  *   def crossover(mate: Chromosome): Chromosome
+  * }}}
+  *
+  * */
 
 trait Chromosome {
+  /** Produces a slightly modified chromosome.
+    *
+    * @return A chromosome
+    */
+
   def mutate: Chromosome
+
+  /** Produces a child chromosome given a mate. The child chromosome reflects
+    * characteristics of both parent chromosomes.
+    *
+    * @param mate A chromosome with which to mate
+    * @return A child chromosome
+    */
 
   def crossover(mate: Chromosome): Chromosome
 }
