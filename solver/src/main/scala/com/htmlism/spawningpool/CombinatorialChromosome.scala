da5a0453ced85eb9f17331605b52021eb9df9bde
Mark Canlas <github@htmlism.com>
2014-07-14 11:54:39 -0400

    made alleles a def

--- solver/src/main/scala/com/htmlism/spawningpool/CombinatorialChromosome.scala
+++ solver/src/main/scala/com/htmlism/spawningpool/CombinatorialChromosome.scala
@@ -1,11 +1,17 @@
 package com.htmlism.spawningpool
 
-/** A trait for chromosomes that support the generation of alleles from a collection of values. */
+/** A trait for chromosomes that support the generation of alleles from a collection of values.
+  *
+  * {{{
+  *   def alleles: Seq[A]
+  * }}}
+  *
+  * */
 
 trait CombinatorialChromosome[A] extends HomogeneousChromosome[A] {
   self: IndexedChromosome[A] =>
 
-  val alleles: Seq[A]
+  def alleles: Seq[A]
 
   def generateAllele = ??? // TODO given rig
 }
