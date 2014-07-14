2a9ea4d8d748f92c36c6925d52b83800ae06739e
Mark Canlas <github@htmlism.com>
2014-07-13 21:01:48 -0400

    renamed allele collection

--- solver/src/main/scala/com/htmlism/spawningpool/CombinatorialChromosome.scala
+++ solver/src/main/scala/com/htmlism/spawningpool/CombinatorialChromosome.scala
@@ -5,7 +5,7 @@ package com.htmlism.spawningpool
 trait CombinatorialChromosome[A] extends HomogeneousChromosome[A] {
   self: IndexedChromosome[A] =>
 
-  val elements: Seq[A]
+  val alleles: Seq[A]
 
   def generateAllele = ??? // TODO given rig
 }
