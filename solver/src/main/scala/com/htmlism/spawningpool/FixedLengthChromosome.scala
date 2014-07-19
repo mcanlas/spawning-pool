450fcd8d200837cf3246bf39a86e18e0926b966c
Mark Canlas <github@htmlism.com>
2014-07-19 18:18:23 -0400

    added type doc

--- solver/src/main/scala/com/htmlism/spawningpool/FixedLengthChromosome.scala
+++ solver/src/main/scala/com/htmlism/spawningpool/FixedLengthChromosome.scala
@@ -1,6 +1,11 @@
 package com.htmlism.spawningpool
 
-/** The base trait for homogeneous chromosomes of a fixed length. */
+/** The base trait for homogeneous chromosomes of a fixed length.
+  *
+  * @tparam A The type for a single gene
+  * @tparam B The trait or class extending this trait
+  *
+  */
 
 trait FixedLengthChromosome[B, A] extends IndexedChromosome[B, A] {
   // an optimization given that both chromosomes are the same length
