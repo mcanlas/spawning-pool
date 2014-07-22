dea6d88c140cf350fc439d89b2ca48f2fa5e0e47
Mark Canlas <github@htmlism.com>
2014-07-22 11:42:49 -0400

    normalized type parameters

--- solver/src/main/scala/com/htmlism/spawningpool/CombinatorialChromosome.scala
+++ solver/src/main/scala/com/htmlism/spawningpool/CombinatorialChromosome.scala
@@ -3,13 +3,16 @@ package com.htmlism.spawningpool
 /** A trait for chromosomes that support the generation of alleles from a collection of values.
   *
   * {{{
-  *   def alleles: Seq[A]
+  *   def alleles: Seq[B]
   * }}}
+  *
+  * @tparam A The trait or class extending this trait
+  * @tparam B The type for a single gene
   *
   * */
 
-trait CombinatorialChromosome[B, A] extends HomogeneousChromosome[B, A] {
-  def alleles: Seq[A]
+trait CombinatorialChromosome[A, B] extends HomogeneousChromosome[A, B] {
+  def alleles: Seq[B]
 
   def generateAllele = alleles(randomAlleleIndex)
 
