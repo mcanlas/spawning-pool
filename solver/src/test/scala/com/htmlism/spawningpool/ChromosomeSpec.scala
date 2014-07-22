44cf18bd4069b00e0bb5251f16277901fb16f13e
Mark Canlas <github@htmlism.com>
2014-07-22 17:28:04 -0400

    implemented variable length tests

--- solver/src/test/scala/com/htmlism/spawningpool/ChromosomeSpec.scala
+++ solver/src/test/scala/com/htmlism/spawningpool/ChromosomeSpec.scala
@@ -17,7 +17,34 @@ class ChromosomeSpec extends Specification {
     }
   }
 
-  // TODO test variable length
+  "A variable-length chromosome" should {
+    import VariableLengthChromosome._
+
+    "support spot mutation" in {
+      val stack = StackOperations(Seq('subtract, 'divide, 'multiply), MutateGene)
+
+      stack.mutate.genes === Seq('subtract, 'conquer, 'multiply)
+    }
+
+    "support add mutation" in {
+      val stack = StackOperations(Seq('subtract, 'divide, 'multiply), AddGene)
+
+      stack.mutate.genes === Seq('subtract, 'divide, 'multiply, 'conquer)
+    }
+
+    "support drop mutation" in {
+      val stack = StackOperations(Seq('subtract, 'divide, 'multiply), RemoveGene)
+
+      stack.mutate.genes === Seq('subtract, 'multiply)
+    }
+
+    "support crossover" in {
+      val firstStack  = StackOperations(Seq('subtract, 'divide, 'multiply), MutateGene)
+      val secondStack = StackOperations(Seq('power, 'log, 'conquer, 'modulus, 'root), MutateGene)
+
+      firstStack.crossover(secondStack).genes === Seq('subtract, 'log, 'multiply, 'modulus)
+    }
+  }
 }
 
 case class TspTour(genes: Seq[Symbol]) extends FixedLengthChromosome[TspTour, Symbol] with CombinatorialChromosome[TspTour, Symbol] {
@@ -29,5 +56,18 @@ case class TspTour(genes: Seq[Symbol]) extends FixedLengthChromosome[TspTour, Sy
 
   override def randomAlleleIndex = 0
   override def randomGeneIndex = 1
+  override def randomThisParent = parents.nextElement
+}
+
+case class StackOperations(genes: Seq[Symbol], override val randomMutationOperation: Int) extends VariableLengthChromosome[StackOperations, Symbol] with CombinatorialChromosome[StackOperations, Symbol] {
+  private val parents = new DeterministicGenerator[Boolean](Seq(true, false, true, false, true))
+
+  def alleles = Seq('conquer)
+
+  def construct(genes: Seq[Symbol]) = StackOperations(genes, randomMutationOperation)
+
+  override def randomAlleleIndex = 0
+  override def randomGeneIndex = 1
+  override def randomNewGeneIndex = 4
   override def randomThisParent = parents.nextElement
 }
