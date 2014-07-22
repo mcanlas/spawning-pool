1af140f7cc756ce79359b97c3b2b25a3f2e9ff7a
Mark Canlas <github@htmlism.com>
2014-07-22 15:26:40 -0400

    made block more concise

--- solver/src/main/scala/com/htmlism/spawningpool/FixedLengthChromosome.scala
+++ solver/src/main/scala/com/htmlism/spawningpool/FixedLengthChromosome.scala
@@ -10,12 +10,7 @@ package com.htmlism.spawningpool
 trait FixedLengthChromosome[A <: FixedLengthChromosome[A, B], B] extends IndexedChromosome[A, B] {
   // an optimization given that both chromosomes are the same length
   override def crossover(mate: A) = {
-    val childGenes = for (n <- 0 to genes.size - 1) yield {
-      if (randomThisParent)
-        this(n)
-      else
-        mate(n)
-    }
+    val childGenes = for (n <- 0 to genes.size - 1) yield (if (randomThisParent) this else mate)(n)
 
     construct(childGenes)
   }
