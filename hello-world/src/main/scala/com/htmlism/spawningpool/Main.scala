59a517dd267d879b8eff2cdc2686a82c3a90c186
Mark Canlas <github@htmlism.com>
2014-07-24 17:08:58 -0400

    added phrase class

--- hello-world/src/main/scala/com/htmlism/spawningpool/Main.scala
+++ hello-world/src/main/scala/com/htmlism/spawningpool/Main.scala
@@ -3,3 +3,20 @@ package com.htmlism.spawningpool
 object Main extends App {
   println("hello")
 }
+
+class Phrase(val genes: Seq[Char]) extends FixedLengthChromosome[Phrase, Char] {
+  /** Generates an allele.
+    *
+    * This method provides a source of values for genes during mutation.
+    *
+    * @return An allele.
+    */
+  def generateAllele = (new util.Random).nextInt(Char.MaxValue.toInt).toChar
+
+  /** A constructor for chromosomes of this type.
+    *
+    * @param genes The genes of the new chromosome
+    * @return A new chromosome
+    */
+  def construct(genes: Seq[Char]) = new Phrase(genes)
+}
