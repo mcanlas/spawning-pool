a0a9f1aeb73e80124dc39637f2b4b773375faf05
Mark Canlas <github@htmlism.com>
2014-10-12 21:24:57 -0400

    things with length have size

--- solver/src/main/scala/com/htmlism/spawningpool/FixedLengthChromosomeGenerator.scala
+++ solver/src/main/scala/com/htmlism/spawningpool/FixedLengthChromosomeGenerator.scala
@@ -1,5 +1,5 @@
 package com.htmlism.spawningpool
 
-class FixedLengthChromosomeGenerator[A <: FixedLengthChromosome[A, B], B] extends ChromosomeGenerator[A] {
+class FixedLengthChromosomeGenerator[A <: FixedLengthChromosome[A, B], B](size: Int) extends ChromosomeGenerator[A] {
   def generateChromosome = ???
 }
