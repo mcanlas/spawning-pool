4ad8b1f0113b29b47916bd36225b1059866b7c79
Mark Canlas <github@htmlism.com>
2014-10-12 21:19:54 -0400

    added chromosome generator stubs

---
+++ solver/src/main/scala/com/htmlism/spawningpool/FixedLengthChromosomeGenerator.scala
@@ -0,0 +1,5 @@
+package com.htmlism.spawningpool
+
+class FixedLengthChromosomeGenerator[A <: FixedLengthChromosome[A, B], B] extends ChromosomeGenerator[A] {
+  def generateChromosome = ???
+}
---
+++ solver/src/main/scala/com/htmlism/spawningpool/VariableLengthChromosomeGenerator.scala
@@ -0,0 +1,5 @@
+package com.htmlism.spawningpool
+
+class VariableLengthChromosomeGenerator[A <: VariableLengthChromosome[A, B], B] extends ChromosomeGenerator[A] {
+  def generateChromosome = ???
+}
