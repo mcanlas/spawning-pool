5014f4277b6c245f17e23ed81873afccc5c6ffef
Mark Canlas <github@htmlism.com>
2014-10-16 18:17:10 -0400

    make ordering implicit

--- solver/src/main/scala/com/htmlism/spawningpool/SolutionContext.scala
+++ solver/src/main/scala/com/htmlism/spawningpool/SolutionContext.scala
@@ -1,11 +1,11 @@
 package com.htmlism.spawningpool
 
 object SolutionContext {
-  def apply[A, B](fitness: A => B, ordering: Ordering[B], population: Seq[A]) = {
-    new SolutionContext(memoize(fitness), ordering, population)
+  def apply[A, B](fitness: A => B, population: Seq[A])(implicit ordering: Ordering[B]) = {
+    new SolutionContext(memoize(fitness), population)
   }
 }
 
-class SolutionContext[A, B](val fitness: A => B, val ordering: Ordering[B], val population: Seq[A]) {
-  def withPopulation(newPopulation: Seq[A]) = new SolutionContext(fitness, ordering, newPopulation)
+class SolutionContext[A, B](val fitness: A => B, val population: Seq[A])(implicit val ordering: Ordering[B]) {
+  def withPopulation(newPopulation: Seq[A]) = new SolutionContext(fitness, newPopulation)
 }
--- solver/src/main/scala/com/htmlism/spawningpool/Solver.scala
+++ solver/src/main/scala/com/htmlism/spawningpool/Solver.scala
@@ -63,7 +63,7 @@ class Solver[A, B](fitnessFunction: A => B, populationSize: Int = 50, islandCoun
   def evolveFrom(seeding: => Population) = Future {
     val islands = generateIslands(seeding)
 
-    val evolvedIslands = islands.map { p => evolvePopulation(SolutionContext(fitnessFunction, ordering, p)) }
+    val evolvedIslands = islands.map { p => evolvePopulation(SolutionContext(fitnessFunction, p)) }
 
     fittestSolutions(evolvedIslands)
   }
