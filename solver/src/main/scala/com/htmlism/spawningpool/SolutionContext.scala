69073bdfa2214f878833a2232f734c8c030e7cc1
Mark Canlas <github@htmlism.com>
2014-10-17 00:44:42 -0400

    added generations count

--- solver/src/main/scala/com/htmlism/spawningpool/SolutionContext.scala
+++ solver/src/main/scala/com/htmlism/spawningpool/SolutionContext.scala
@@ -2,10 +2,10 @@ package com.htmlism.spawningpool
 
 object SolutionContext {
   def apply[A, B](fitness: A => B, evolver: Evolver[A], population: Seq[A])(implicit ordering: Ordering[B]) = {
-    new SolutionContext(fitness, evolver, population)
+    new SolutionContext(fitness, evolver, population, 0)
   }
 }
 
-class SolutionContext[A, B](val fitness: A => B, val evolver: Evolver[A],val population: Seq[A])(implicit val ordering: Ordering[B]) {
-  def withPopulation(newPopulation: Seq[A]) = new SolutionContext(fitness, evolver, newPopulation)
+class SolutionContext[A, B](val fitness: A => B, val evolver: Evolver[A], val population: Seq[A], val generations: Int)(implicit val ordering: Ordering[B]) {
+  def increment(newPopulation: Seq[A]) = new SolutionContext(fitness, evolver, newPopulation, generations + 1)
 }
