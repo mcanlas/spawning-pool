2791b93259c734e8ae14b40add271124fbcec71e
Mark Canlas <github@htmlism.com>
2014-10-17 22:15:41 -0400

    annotated public methods (mostly for intellij)

--- solver/src/main/scala/com/htmlism/spawningpool/SolutionContext.scala
+++ solver/src/main/scala/com/htmlism/spawningpool/SolutionContext.scala
@@ -1,11 +1,11 @@
 package com.htmlism.spawningpool
 
 object SolutionContext {
-  def apply[A, B](id: Int, fitness: A => B, evolver: Evolver[A], population: Seq[A])(implicit ordering: Ordering[B]) = {
+  def apply[A, B](id: Int, fitness: A => B, evolver: Evolver[A], population: Seq[A])(implicit ordering: Ordering[B]): SolutionContext[A, B] = {
     new SolutionContext(id, fitness, evolver, population, 0)
   }
 }
 
 class SolutionContext[A, B](val id: Int, val fitness: A => B, val evolver: Evolver[A], val population: Seq[A], val generations: Int)(implicit val ordering: Ordering[B]) {
-  def increment(newPopulation: Seq[A]) = new SolutionContext(id, fitness, evolver, newPopulation, generations + 1)
+  def increment(newPopulation: Seq[A]) = new SolutionContext(id, fitness, evolver, newPopulation, generations + 1): SolutionContext[A, B]
 }
