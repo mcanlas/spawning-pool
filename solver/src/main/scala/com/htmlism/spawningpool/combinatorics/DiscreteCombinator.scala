c56cea636d1bc7853a69b242d99c4c231e7c6200
Mark Canlas <github@htmlism.com>
2014-10-22 09:16:57 -0400

    added combinator

---
+++ solver/src/main/scala/com/htmlism/spawningpool/combinatorics/DiscreteCombinator.scala
@@ -0,0 +1,15 @@
+package com.htmlism.spawningpool.combinatorics
+
+import com.htmlism.spawningpool.RandomIndexProvider
+
+object DiscreteCombinator {
+  def fixed[A, B](elements: Seq[A], builder: Traversable[A] => B, size: Int) = ???
+
+  def variable[A, B](elements: Seq[A], builder: Traversable[A] => B, upperBound: Int) = ???
+}
+
+class DiscreteCombinator[A, B](builder: Traversable[B] => A, val alleles: Seq[B])(implicit rip: RandomIndexProvider) extends DiscreteAlleleGenerator[B] with HomogenousCombinator[A, B] {
+  def randomIndex(size: Int) = rip.randomIndex(size)
+
+  def build(chromosomes: Traversable[B]) = builder(chromosomes)
+}
