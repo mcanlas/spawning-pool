5d1a22787c9b12138a847f83f5818bf60e25da06
Mark Canlas <github@htmlism.com>
2014-10-21 21:58:07 -0400

    made evolvers more generic

--- solver/src/main/scala/com/htmlism/spawningpool/combinatorics/FixedLengthEvolver.scala
+++ solver/src/main/scala/com/htmlism/spawningpool/combinatorics/FixedLengthEvolver.scala
@@ -2,7 +2,7 @@ package com.htmlism.spawningpool.combinatorics
 
 import com.htmlism.spawningpool.Evolver
 
-class FixedLengthEvolver[A <: FixedLengthChromosome[A, _]] extends Evolver[A] {
+trait FixedLengthEvolver[A <: FixedLengthChromosome[A, _]] extends Evolver[A] {
   def mutate(chromosome: A) = ???
 
   def crossover(firstParent: A, secondParent: A) = ???
--- solver/src/main/scala/com/htmlism/spawningpool/combinatorics/VariableLengthEvolver.scala
+++ solver/src/main/scala/com/htmlism/spawningpool/combinatorics/VariableLengthEvolver.scala
@@ -2,7 +2,7 @@ package com.htmlism.spawningpool.combinatorics
 
 import com.htmlism.spawningpool.Evolver
 
-class VariableLengthEvolver[A <: VariableLengthChromosome[A, _]] extends Evolver[A] {
+trait VariableLengthEvolver[A <: VariableLengthChromosome[A, _]] extends Evolver[A] {
   def mutate(chromosome: A) = ???
 
   def crossover(firstParent: A, secondParent: A) = ???
