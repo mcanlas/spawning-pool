8b65c6b646de36d76066ad8fb560048ad7631925
Mark Canlas <github@htmlism.com>
2014-10-23 22:22:57 -0400

    made homos have all the things

--- solver/src/main/scala/com/htmlism/spawningpool/combinatorics/DiscreteCombinator.scala
+++ solver/src/main/scala/com/htmlism/spawningpool/combinatorics/DiscreteCombinator.scala
@@ -1,13 +1,9 @@
 package com.htmlism.spawningpool.combinatorics
 
-import com.htmlism.spawningpool.RandomIndexProvider
-
 object DiscreteCombinator {
   def fixed[A](elements: Seq[A], size: Int) = ???
 
   def variable[A](elements: Seq[A], upperBound: Int) = ???
 }
 
-class DiscreteCombinator[A, B](val alleles: Seq[B])(implicit rip: RandomIndexProvider) extends DiscreteAlleleGenerator[B] with HomogenousCombinator[A, B] {
-  def randomIndex(size: Int) = rip.randomIndex(size)
-}
+trait DiscreteCombinator[A] extends DiscreteAlleleGenerator[A] with HomogenousCombinator[A]
--- solver/src/main/scala/com/htmlism/spawningpool/combinatorics/HomogenousCombinator.scala
+++ solver/src/main/scala/com/htmlism/spawningpool/combinatorics/HomogenousCombinator.scala
@@ -1,10 +1,12 @@
 package com.htmlism.spawningpool.combinatorics
 
+import com.htmlism.spawningpool.{ ChromosomeGenerator, Evolver }
+
 object HomogenousCombinator {
   def fixed[A](alleles: Seq[A], size: Int) = ???
 
   def variable[A](alleles: Seq[A], upperBound: Int) = ???
 }
 
-trait HomogenousCombinator[A, B] {
+trait HomogenousCombinator[A] extends AlleleGenerator[A] with ChromosomeGenerator[Seq[A]] with Evolver[Seq[A]] {
 }
