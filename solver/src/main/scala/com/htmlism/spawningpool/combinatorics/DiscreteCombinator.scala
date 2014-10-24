732673340c2752cab166b9326f3abc90d56c30fd
Mark Canlas <github@htmlism.com>
2014-10-23 21:48:40 -0400

    dropped builders

--- solver/src/main/scala/com/htmlism/spawningpool/combinatorics/DiscreteCombinator.scala
+++ solver/src/main/scala/com/htmlism/spawningpool/combinatorics/DiscreteCombinator.scala
@@ -3,11 +3,11 @@ package com.htmlism.spawningpool.combinatorics
 import com.htmlism.spawningpool.RandomIndexProvider
 
 object DiscreteCombinator {
-  def fixed[A, B](elements: Seq[A], builder: Traversable[A] => B, size: Int) = ???
+  def fixed[A](elements: Seq[A], size: Int) = ???
 
-  def variable[A, B](elements: Seq[A], builder: Traversable[A] => B, upperBound: Int) = ???
+  def variable[A](elements: Seq[A], upperBound: Int) = ???
 }
 
-class DiscreteCombinator[A, B](builder: Traversable[B] => A, val alleles: Seq[B])(implicit rip: RandomIndexProvider) extends DiscreteAlleleGenerator[B] with HomogenousCombinator[A, B] {
+class DiscreteCombinator[A, B](val alleles: Seq[B])(implicit rip: RandomIndexProvider) extends DiscreteAlleleGenerator[B] with HomogenousCombinator[A, B] {
   def randomIndex(size: Int) = rip.randomIndex(size)
 }
