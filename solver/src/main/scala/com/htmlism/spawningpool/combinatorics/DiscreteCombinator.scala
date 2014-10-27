973204b7905a5d90d853e3213b930106b266b6c8
Mark Canlas <github@htmlism.com>
2014-10-27 15:33:41 -0400

    dropped unused trait

--- solver/src/main/scala/com/htmlism/spawningpool/combinatorics/DiscreteCombinator.scala
+++
@@ -1,9 +0,0 @@
-package com.htmlism.spawningpool.combinatorics
-
-object DiscreteCombinator {
-  def fixed[A](elements: Seq[A], size: Int) = ???
-
-  def variable[A](elements: Seq[A], upperBound: Int) = ???
-}
-
-trait DiscreteCombinator[A <: Seq[B], B] extends DiscreteAlleleGenerator[B] with HomogenousCombinator[A, B]
