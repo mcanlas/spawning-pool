ada96e4874796f64bcd4bbcea95265925444be31
Mark Canlas <github@htmlism.com>
2014-10-27 15:38:42 -0400

    dropped unused factory

--- solver/src/main/scala/com/htmlism/spawningpool/combinatorics/HomogenousCombinator.scala
+++ solver/src/main/scala/com/htmlism/spawningpool/combinatorics/HomogenousCombinator.scala
@@ -2,12 +2,6 @@ package com.htmlism.spawningpool.combinatorics
 
 import com.htmlism.spawningpool.{ ChromosomeGenerator, Evolver }
 
-object HomogenousCombinator {
-  def fixed[A](alleles: Seq[A], size: Int) = ???
-
-  def variable[A](alleles: Seq[A], upperBound: Int) = ???
-}
-
 trait HomogenousCombinator[A <: Seq[B], B] extends AlleleGenerator[B] with ChromosomeGenerator[A] with Evolver[A] {
   def mutate(chromosome: A) = ???
 
