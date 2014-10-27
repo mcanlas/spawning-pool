149bf1376f608152863744e901db3ee37b40c632
Mark Canlas <github@htmlism.com>
2014-10-27 15:26:50 -0400

    added discrete combinators

---
+++ solver/src/main/scala/com/htmlism/spawningpool/combinatorics/DiscreteVariableLengthCombinator.scala
@@ -0,0 +1,13 @@
+package com.htmlism.spawningpool.combinatorics
+
+class DiscreteVariableLengthCombinator[A <: Seq[B], B] extends DiscreteCombinator[A, B] {
+  override def mutate(chromosome: A) = ???
+
+  override def crossover(firstParent: A, secondParent: A) = ???
+
+  def alleles = ???
+
+  def generateChromosome = ???
+
+  def randomIndex(size: Int) = ???
+}
