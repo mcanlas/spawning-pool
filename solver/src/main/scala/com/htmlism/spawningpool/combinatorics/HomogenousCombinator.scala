001f775c328e83131ae5c8587317baee7a3ef28b
Mark Canlas <github@htmlism.com>
2014-10-29 00:42:05 -0400

    factor out container type for maximum nerdity

--- solver/src/main/scala/com/htmlism/spawningpool/combinatorics/HomogenousCombinator.scala
+++ solver/src/main/scala/com/htmlism/spawningpool/combinatorics/HomogenousCombinator.scala
@@ -2,6 +2,10 @@ package com.htmlism.spawningpool.combinatorics
 
 import com.htmlism.spawningpool.{ ChromosomeGenerator, Evolver }
 
+object HomogenousCombinator {
+  val SequenceFactory = Vector
+}
+
 trait HomogenousCombinator[A, B <: Seq[A]] extends AlleleGenerator[A]
   with ChromosomeGenerator[B]
   with Evolver[B]
