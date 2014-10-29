c7fd130559be41c19cb5a0afbbdc33bd3a90b861
Mark Canlas <github@htmlism.com>
2014-10-29 01:52:59 -0400

    dropped unused provider

--- solver/src/test/scala/com/htmlism/spawningpool/ChromosomeProvider.scala
+++
@@ -1,7 +0,0 @@
-package com.htmlism.spawningpool
-
-class ChromosomeProvider[A](chromosomes: Traversable[A]) extends DeterministicProvider[A](chromosomes) with ChromosomeGenerator[A] {
-  def this(chromosomes: A*) = this(chromosomes)
-
-  def generateChromosome = nextElement
-}
