d4cc989b8e54b49783f2317e71aece0687de20ff
Mark Canlas <github@htmlism.com>
2014-10-28 23:49:47 -0400

    moved alleles to constructor

--- solver/src/main/scala/com/htmlism/spawningpool/combinatorics/DiscreteFixedLengthCombinator.scala
+++ solver/src/main/scala/com/htmlism/spawningpool/combinatorics/DiscreteFixedLengthCombinator.scala
@@ -1,5 +1,7 @@
 package com.htmlism.spawningpool.combinatorics
 
-class DiscreteFixedLengthCombinator[A, B <: Seq[A]] extends FixedLengthCombinator[A, B] with DiscreteAlleleGenerator[A] with DefaultRandomProvider {
-  def alleles = ???
+class DiscreteFixedLengthCombinator[A, B <: Seq[A]](val alleles: Seq[A])
+  extends FixedLengthCombinator[A, B]
+  with DiscreteAlleleGenerator[A]
+  with DefaultRandomProvider {
 }
--- solver/src/main/scala/com/htmlism/spawningpool/combinatorics/DiscreteVariableLengthCombinator.scala
+++ solver/src/main/scala/com/htmlism/spawningpool/combinatorics/DiscreteVariableLengthCombinator.scala
@@ -1,5 +1,7 @@
 package com.htmlism.spawningpool.combinatorics
 
-class DiscreteVariableLengthCombinator[A, B <: Seq[A]] extends VariableLengthCombinator[A, B] with DiscreteAlleleGenerator[A] with DefaultRandomProvider {
-  def alleles = ???
+class DiscreteVariableLengthCombinator[A, B <: Seq[A]](val alleles: Seq[A])
+  extends VariableLengthCombinator[A, B]
+  with DiscreteAlleleGenerator[A]
+  with DefaultRandomProvider {
 }
