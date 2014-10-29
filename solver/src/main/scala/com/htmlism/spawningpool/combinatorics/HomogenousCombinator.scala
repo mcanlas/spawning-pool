ab08f93b4227b7c517e4d6447efe5b09e3d55b06
Mark Canlas <github@htmlism.com>
2014-10-28 22:48:11 -0400

    added provider inheritance

--- solver/src/main/scala/com/htmlism/spawningpool/combinatorics/HomogenousCombinator.scala
+++ solver/src/main/scala/com/htmlism/spawningpool/combinatorics/HomogenousCombinator.scala
@@ -2,7 +2,11 @@ package com.htmlism.spawningpool.combinatorics
 
 import com.htmlism.spawningpool.{ ChromosomeGenerator, Evolver }
 
-trait HomogenousCombinator[A <: Seq[B], B] extends AlleleGenerator[B] with ChromosomeGenerator[A] with Evolver[A] {
+trait HomogenousCombinator[A <: Seq[B], B] extends AlleleGenerator[B]
+  with ChromosomeGenerator[A]
+  with Evolver[A]
+  with GeneIndexProvider
+  with CrossoverParentProvider {
   def mutate(chromosome: A) = ???
 
   def crossover(firstParent: A, secondParent: A) = ???
--- solver/src/main/scala/com/htmlism/spawningpool/combinatorics/VariableLengthCombinator.scala
+++ solver/src/main/scala/com/htmlism/spawningpool/combinatorics/VariableLengthCombinator.scala
@@ -1,6 +1,6 @@
 package com.htmlism.spawningpool.combinatorics
 
-trait VariableLengthCombinator[A <: Seq[B], B] extends HomogenousCombinator[A, B] {
+trait VariableLengthCombinator[A <: Seq[B], B] extends HomogenousCombinator[A, B] with MutationMethodProvider {
   override def mutate(chromosome: A) = ???
 
   override def crossover(firstParent: A, secondParent: A) = ???
