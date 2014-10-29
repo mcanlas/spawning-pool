86568072f8eadfb6dfe640afa3b01597bc10bf55
Mark Canlas <github@htmlism.com>
2014-10-28 21:48:49 -0400

    dropped unused index provider

--- solver/src/test/scala/com/htmlism/spawningpool/IndexProvider.scala
+++
@@ -1,7 +0,0 @@
-package com.htmlism.spawningpool
-
-class IndexProvider(indices: Traversable[Int]) extends DeterministicProvider[Int](indices) with RandomIndexProvider {
-  def this(indices: Int*) = this(indices)
-
-  def randomIndex(size: Int) = nextElement
-}
