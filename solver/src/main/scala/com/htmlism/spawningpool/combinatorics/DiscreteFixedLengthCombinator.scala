685fc22caf3ff8d13cbb1b9d23b9dd6c06289377
Mark Canlas <github@htmlism.com>
2014-10-28 23:30:59 -0400

    dropped unneeded bottom overrides

--- solver/src/main/scala/com/htmlism/spawningpool/combinatorics/DiscreteFixedLengthCombinator.scala
+++ solver/src/main/scala/com/htmlism/spawningpool/combinatorics/DiscreteFixedLengthCombinator.scala
@@ -1,11 +1,5 @@
 package com.htmlism.spawningpool.combinatorics
 
 class DiscreteFixedLengthCombinator[A <: Seq[B], B] extends FixedLengthCombinator[A, B] with DiscreteAlleleGenerator[B] with DefaultRandomProvider {
-  override def mutate(chromosome: A) = ???
-
-  override def crossover(firstParent: A, secondParent: A) = ???
-
   def alleles = ???
-
-  override def generateChromosome = ???
 }
