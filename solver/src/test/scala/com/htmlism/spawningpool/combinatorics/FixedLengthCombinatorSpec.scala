e9be3e664a117c2e0f9f067278e42b36a2237faf
Mark Canlas <github@htmlism.com>
2014-11-01 14:24:45 -0400

    added should stubs

--- solver/src/test/scala/com/htmlism/spawningpool/combinatorics/FixedLengthCombinatorSpec.scala
+++ solver/src/test/scala/com/htmlism/spawningpool/combinatorics/FixedLengthCombinatorSpec.scala
@@ -3,4 +3,6 @@ package com.htmlism.spawningpool.combinatorics
 import org.specs2.mutable.Specification
 
 class FixedLengthCombinatorSpec extends Specification {
+  "A fixed-length generator" should {
+  }
 }
--- solver/src/test/scala/com/htmlism/spawningpool/combinatorics/VariableLengthCombinatorSpec.scala
+++ solver/src/test/scala/com/htmlism/spawningpool/combinatorics/VariableLengthCombinatorSpec.scala
@@ -3,4 +3,6 @@ package com.htmlism.spawningpool.combinatorics
 import org.specs2.mutable.Specification
 
 class VariableLengthCombinatorSpec extends Specification {
+  "A variable-length generator" should {
+  }
 }
