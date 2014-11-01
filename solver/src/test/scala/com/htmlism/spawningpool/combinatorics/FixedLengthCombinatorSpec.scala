7219c6526dde5006b92f4955785c7925d62d011a
Mark Canlas <github@htmlism.com>
2014-11-01 15:30:22 -0400

    added tests

--- solver/src/test/scala/com/htmlism/spawningpool/combinatorics/FixedLengthCombinatorSpec.scala
+++ solver/src/test/scala/com/htmlism/spawningpool/combinatorics/FixedLengthCombinatorSpec.scala
@@ -4,10 +4,33 @@ import org.specs2.mutable.Specification
 
 class FixedLengthCombinatorSpec extends Specification {
   "A fixed-length generator" should {
+    val size = 3
+
+    val generator = new FixedTestCombinator(size)
+
+    val firstChromosome  = generator.generateChromosome
+    val secondChromosome = generator.generateChromosome
+
+    "generate chromosomes of a fixed length" in {
+      firstChromosome.length  === size
+      secondChromosome.length === size
+    }
+
+    "generate the expected values" in {
+      firstChromosome  === Seq('luigi, 'bowser, 'peach)
+      secondChromosome === Seq('mario, 'luigi, 'bowser)
+    }
   }
 }
 
-// TODO drop the abstract and attempt to implement this in intellij
-abstract class FixedTestCombinator
-  extends HomogenousCombinator[Symbol, Vector[Symbol]]
-  with DiscreteAlleleGenerator[Symbol]
+class FixedTestCombinator(val size: Int)
+  extends FixedLengthCombinator[Symbol]
+  with DiscreteAlleleGenerator[Symbol] {
+  private val alleleIndexes = Iterable(1, 3, 2, 0, 1, 3).iterator
+
+  def alleles: Seq[Symbol] = Seq('mario, 'luigi, 'peach, 'bowser)
+
+  def randomDiscreteAlleleIndex(size: Int) = alleleIndexes.next()
+
+  override def fill(size: Int) = List.fill(size)
+}
