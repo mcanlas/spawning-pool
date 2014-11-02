5c18d7c2e2a81510adc93ed630c09d39ac68560e
Mark Canlas <github@htmlism.com>
2014-11-02 12:02:48 -0500

    dropped dedicated generators

--- solver/src/main/scala/com/htmlism/spawningpool/combinatorics/FixedLengthChromosomeGenerator.scala
+++
@@ -1,7 +0,0 @@
-package com.htmlism.spawningpool.combinatorics
-
-import com.htmlism.spawningpool.ChromosomeGenerator
-
-class FixedLengthChromosomeGenerator[A <: FixedLengthChromosome[A, B], B](elements: Set[B], size: Int) extends ChromosomeGenerator[A] {
-  def generateChromosome = ???
-}
--- solver/src/main/scala/com/htmlism/spawningpool/combinatorics/VariableLengthChromosomeGenerator.scala
+++
@@ -1,7 +0,0 @@
-package com.htmlism.spawningpool.combinatorics
-
-import com.htmlism.spawningpool.ChromosomeGenerator
-
-class VariableLengthChromosomeGenerator[A <: VariableLengthChromosome[A, B], B](elements: Set[B]) extends ChromosomeGenerator[A] {
-  def generateChromosome = ???
-}
