ba1d5779d6dc6d491883799ca4607dea9ddfa56b
Mark Canlas <github@htmlism.com>
2014-11-03 22:55:49 -0500

    added variable length constants

--- solver/src/main/scala/com/htmlism/spawningpool/combinatorics/VariableLengthCombinator.scala
+++ solver/src/main/scala/com/htmlism/spawningpool/combinatorics/VariableLengthCombinator.scala
@@ -1,5 +1,11 @@
 package com.htmlism.spawningpool.combinatorics
 
+object VariableLengthCombinator {
+  val MutateGene = 0
+  val AddGene    = 1
+  val RemoveGene = 2
+}
+
 trait VariableLengthCombinator[A]
   extends HomogenousCombinator[A]
   with MutationMethodProvider {
