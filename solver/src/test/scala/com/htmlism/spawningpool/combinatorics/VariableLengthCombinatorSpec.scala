2ef34e660cd8b910517225a4c4043fb33c46ac1a
Mark Canlas <github@htmlism.com>
2014-11-06 23:41:29 -0500

    added test class

--- solver/src/test/scala/com/htmlism/spawningpool/combinatorics/VariableLengthCombinatorSpec.scala
+++ solver/src/test/scala/com/htmlism/spawningpool/combinatorics/VariableLengthCombinatorSpec.scala
@@ -6,3 +6,19 @@ class VariableLengthCombinatorSpec extends Specification {
   "A variable-length generator" should {
   }
 }
+
+class VariableTestCombinator
+  extends VariableLengthCombinator[Symbol]
+  with DiscreteAlleleGenerator[Symbol] {
+  def maximumSize: Int = ???
+
+  def alleles: Seq[Symbol] = ???
+
+  def nextMutationMethod: Int = ???
+
+  def nextAlleleIndex(size: Int): Int = ???
+
+  def nextUseFirstParent: Boolean = ???
+
+  def nextGeneIndex(size: Int): Int = ???
+}
