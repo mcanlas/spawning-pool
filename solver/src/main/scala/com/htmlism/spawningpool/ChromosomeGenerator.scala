b08749036506abaca2139b3ad6eaa1ec6b868c8c
Mark Canlas <github@htmlism.com>
2014-11-07 09:15:47 -0500

    added scaladoc

--- solver/src/main/scala/com/htmlism/spawningpool/ChromosomeGenerator.scala
+++ solver/src/main/scala/com/htmlism/spawningpool/ChromosomeGenerator.scala
@@ -1,5 +1,11 @@
 package com.htmlism.spawningpool
 
+/**
+ * A base trait for sources that can generate chromosomes
+ *
+ * @tparam A The type of the chromosome
+ */
+
 trait ChromosomeGenerator[A] {
   def generateChromosome: A
 }
--- solver/src/main/scala/com/htmlism/spawningpool/Evolver.scala
+++ solver/src/main/scala/com/htmlism/spawningpool/Evolver.scala
@@ -1,7 +1,32 @@
 package com.htmlism.spawningpool
 
+/**
+ * A base trait for genetic operators
+ *
+ * @tparam A The type of the chromosome
+ */
+
 trait Evolver[A] {
+  /**
+   * Mutates the given chromosome
+   *
+   * The intent of mutation is to provide a small, random change to an existing chromosome and yield a new chromosome whose fitness is somewhat similar to the original
+   *
+   * @param chromosome The chromosome to mutate
+   * @return A new chromosome
+   */
+
   def mutate(chromosome: A): A
+
+  /**
+   * Combines two parent chromosomes to yield a child chromosome
+   *
+   * The intent of combination is to produce a chromosome whose fitness reflects the fitness of parts of its parents
+   *
+   * @param firstParent A parent chromosome
+   * @param secondParent Another parent chromosome
+   * @return A new chromosome
+   */
 
   def crossover(firstParent: A, secondParent: A): A
 }
