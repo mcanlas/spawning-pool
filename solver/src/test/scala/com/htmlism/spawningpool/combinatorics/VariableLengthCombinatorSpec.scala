695e0b7ade4fd1de5223b5da0027010672c68d14
Mark Canlas <github@htmlism.com>
2014-11-16 22:25:55 -0500

    implemented variable mutation tests

--- solver/src/test/scala/com/htmlism/spawningpool/combinatorics/VariableLengthCombinatorSpec.scala
+++ solver/src/test/scala/com/htmlism/spawningpool/combinatorics/VariableLengthCombinatorSpec.scala
@@ -10,9 +10,9 @@ class VariableLengthCombinatorSpec extends Specification {
     val secondChromosome = combinator.generateChromosome
     val thirdChromosome  = combinator.generateChromosome
 
-    lazy val firstMutation   = combinator.mutate(firstChromosome)
-    lazy val secondMutation  = combinator.mutate(firstChromosome)
-    lazy val thirdtMutation  = combinator.mutate(firstChromosome)
+    val firstMutation  = combinator.mutate(firstChromosome)
+    val secondMutation = combinator.mutate(secondChromosome)
+    val thirdMutation  = combinator.mutate(thirdChromosome)
 
     "generate chromosomes of differing lengths" in {
       firstChromosome.length  === 1
@@ -24,6 +24,18 @@ class VariableLengthCombinatorSpec extends Specification {
       firstChromosome  === Seq('GoGo)
       secondChromosome === Seq('HoneyLemon, 'Baymax, 'Fred)
       thirdChromosome  === Seq('Hiro, 'Wasabi, 'GoGo, 'Baymax, 'HoneyLemon)
+    }
+
+    "support spot mutation" in {
+      secondMutation === Seq('HoneyLemon, 'Hiro, 'Fred)
+    }
+
+    "support insertion mutation" in {
+      thirdMutation === Seq('Wasabi, 'Hiro, 'Wasabi, 'GoGo, 'Baymax, 'HoneyLemon)
+    }
+
+    "support removal mutation" in {
+      firstMutation === Seq.empty
     }
   }
 }
@@ -37,12 +49,12 @@ class VariableTestCombinator
   private val mutationMethods = Iterable(2, 0, 1).iterator
   def nextMutationMethod: Int = mutationMethods.next()
 
-  private val alleleIndexes = Iterable(3, 5, 1, 2, 0, 4, 3, 1, 5).iterator
+  private val alleleIndexes = Iterable(3, 5, 1, 2, 0, 4, 3, 1, 5, 0, 4).iterator
   def nextAlleleIndex(size: Int) = alleleIndexes.next()
 
   def nextUseFirstParent: Boolean = ???
 
-  def nextGeneIndex(size: Int): Int = ???
+  def nextGeneIndex(size: Int): Int = 1
 
   private val lengths = Iterable(1, 3, 5).iterator
   def nextLength(maximum: Int) = lengths.next()
