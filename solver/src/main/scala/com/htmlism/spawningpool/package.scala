05b68da1f5e461eb66d354c0764540bc57562e01
Mark Canlas <github@htmlism.com>
2014-11-17 00:58:45 -0500

    added package description

--- solver/src/main/scala/com/htmlism/spawningpool/package.scala
+++ solver/src/main/scala/com/htmlism/spawningpool/package.scala
@@ -1,5 +1,13 @@
 package com.htmlism
 
+/**
+ * The `spawning-pool` framework enables the search and generation of solutions encoded as a chromosome over a fitness
+ * landscape, all in a generic manner typical of Scala libraries.
+ *
+ * Because genetic algorithms are computationally intensive, this framework uses futures to perform evolution
+ * concurrently.
+ */
+
 package object spawningpool {
   implicit object DefaultRandomIndexProvider extends RandomIndexProvider {
     private val rng = new util.Random
