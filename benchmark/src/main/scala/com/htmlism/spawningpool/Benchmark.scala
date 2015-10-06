831b58e142afa564b0fa4ff57668db42aa37bcf9
Mark Canlas <github@htmlism.com>
2015-10-05 22:23:43 -0400

    added benchmark

---
+++ benchmark/src/main/scala/com/htmlism/spawningpool/Benchmark.scala
@@ -0,0 +1,25 @@
+package com.htmlism.spawningpool
+
+object Benchmark {
+  def apply(f: () => Unit, times: Int): Unit = {
+    val durations = for (n <- 1 to times) yield {
+      println(s"Running $n...")
+
+      val start = compat.Platform.currentTime
+
+      f()
+
+      val duration = start - compat.Platform.currentTime
+      val duratinInSeconds = duration / 1000
+
+      println(s"Duration was $duratinInSeconds seconds")
+      println()
+
+      duration
+    }
+
+    val averageDurationInSeconds = durations.sum / times / 1000
+
+    println(s"Average duration was $averageDurationInSeconds seconds")
+  }
+}
--- build.sbt
+++ build.sbt
@@ -4,6 +4,8 @@ scalaVersion := "2.11.7"
 
 lazy val solver = project
 
+lazy val benchmark = project dependsOn solver
+
 crossScalaVersions := Seq("2.10.5", "2.11.7")
 
 releaseSettings
