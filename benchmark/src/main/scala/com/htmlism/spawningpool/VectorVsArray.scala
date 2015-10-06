c587d1918be327930f28e3586f9c8578ab49f22a
Mark Canlas <github@htmlism.com>
2015-10-06 00:00:53 -0400

    whitespace

--- benchmark/src/main/scala/com/htmlism/spawningpool/VectorVsArray.scala
+++ benchmark/src/main/scala/com/htmlism/spawningpool/VectorVsArray.scala
@@ -27,10 +27,10 @@ object VectorVsArray extends App {
     implicit val combinator = new specialized.DiscreteFixedLengthCombinator(1 to 100, 100)
 
     val solver = new Solver[Seq[Int], Int](
-    { _ => util.Random.nextInt() },
-    islandCount = PositiveCount(15),
-    populationSize = PositiveCount(1000),
-    generations = PositiveCount(40))
+      { _ => util.Random.nextInt() },
+      islandCount = PositiveCount(15),
+      populationSize = PositiveCount(1000),
+      generations = PositiveCount(40))
 
     solver.solveNow
   }
@@ -39,10 +39,10 @@ object VectorVsArray extends App {
     implicit val combinator = new DiscreteFixedLengthCombinator((1 to 100).map(WrappedInt), 100)
 
     val solver = new Solver[Seq[WrappedInt], Int](
-    { _ => util.Random.nextInt() },
-    islandCount = PositiveCount(15),
-    populationSize = PositiveCount(1000),
-    generations = PositiveCount(40))
+      { _ => util.Random.nextInt() },
+      islandCount = PositiveCount(15),
+      populationSize = PositiveCount(1000),
+      generations = PositiveCount(40))
 
     solver.solveNow
   }
@@ -51,10 +51,10 @@ object VectorVsArray extends App {
     implicit val combinator = new specialized.DiscreteFixedLengthCombinator((1 to 100).map(WrappedInt), 100)
 
     val solver = new Solver[Seq[WrappedInt], Int](
-    { _ => util.Random.nextInt() },
-    islandCount = PositiveCount(15),
-    populationSize = PositiveCount(1000),
-    generations = PositiveCount(40))
+      { _ => util.Random.nextInt() },
+      islandCount = PositiveCount(15),
+      populationSize = PositiveCount(1000),
+      generations = PositiveCount(40))
 
     solver.solveNow
   }
