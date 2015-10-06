3b93aac62efabfff18ef59506cfb31e5228bc645
Mark Canlas <github@htmlism.com>
2015-10-05 23:58:07 -0400

    added test

---
+++ benchmark/src/main/scala/com/htmlism/spawningpool/VectorVsArray.scala
@@ -0,0 +1,63 @@
+package com.htmlism.spawningpool
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import com.htmlism.spawningpool.combinatorics._
+
+object VectorVsArray extends App {
+  Benchmark(Map(
+    "vector of ref" -> withVector,
+    "vector of int" -> withVectorInt,
+    "array of ref" -> withArray,
+    "array of int" -> withArrayInt), 10)
+
+  def withVectorInt(): Unit = {
+    implicit val combinator = new DiscreteFixedLengthCombinator(1 to 100, 100)
+
+    val solver = new Solver[Seq[Int], Int](
+      { _ => util.Random.nextInt() },
+      islandCount = PositiveCount(15),
+      populationSize = PositiveCount(1000),
+      generations = PositiveCount(40))
+
+    solver.solveNow
+  }
+
+  def withArrayInt(): Unit = {
+    implicit val combinator = new specialized.DiscreteFixedLengthCombinator(1 to 100, 100)
+
+    val solver = new Solver[Seq[Int], Int](
+    { _ => util.Random.nextInt() },
+    islandCount = PositiveCount(15),
+    populationSize = PositiveCount(1000),
+    generations = PositiveCount(40))
+
+    solver.solveNow
+  }
+
+  def withVector(): Unit = {
+    implicit val combinator = new DiscreteFixedLengthCombinator((1 to 100).map(WrappedInt), 100)
+
+    val solver = new Solver[Seq[WrappedInt], Int](
+    { _ => util.Random.nextInt() },
+    islandCount = PositiveCount(15),
+    populationSize = PositiveCount(1000),
+    generations = PositiveCount(40))
+
+    solver.solveNow
+  }
+
+  def withArray(): Unit = {
+    implicit val combinator = new specialized.DiscreteFixedLengthCombinator((1 to 100).map(WrappedInt), 100)
+
+    val solver = new Solver[Seq[WrappedInt], Int](
+    { _ => util.Random.nextInt() },
+    islandCount = PositiveCount(15),
+    populationSize = PositiveCount(1000),
+    generations = PositiveCount(40))
+
+    solver.solveNow
+  }
+}
+
+case class WrappedInt(a: Int)
