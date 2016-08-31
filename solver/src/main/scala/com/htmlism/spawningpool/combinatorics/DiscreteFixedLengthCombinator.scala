7361dbb93aac1b3a1dedd79e5c6d2847606b1179
Mark Canlas <github@htmlism.com>
2016-08-31 11:44:12 -0400

    dropped empty braces

--- solver/src/main/scala/com/htmlism/spawningpool/combinatorics/DiscreteFixedLengthCombinator.scala
+++ solver/src/main/scala/com/htmlism/spawningpool/combinatorics/DiscreteFixedLengthCombinator.scala
@@ -3,5 +3,4 @@ package com.htmlism.spawningpool.combinatorics
 class DiscreteFixedLengthCombinator[A](val alleles: Seq[A], val size: Int)
   extends FixedLengthCombinator[A]
   with DiscreteAlleleGenerator[A]
-  with DefaultRandomProvider {
-}
+  with DefaultRandomProvider
--- solver/src/main/scala/com/htmlism/spawningpool/combinatorics/DiscreteVariableLengthCombinator.scala
+++ solver/src/main/scala/com/htmlism/spawningpool/combinatorics/DiscreteVariableLengthCombinator.scala
@@ -3,5 +3,4 @@ package com.htmlism.spawningpool.combinatorics
 class DiscreteVariableLengthCombinator[A](val alleles: Seq[A], val initialSize: Int)
   extends VariableLengthCombinator[A]
   with DiscreteAlleleGenerator[A]
-  with DefaultRandomProvider {
-}
+  with DefaultRandomProvider
