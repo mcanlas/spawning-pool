c834c913b27922a6da9a0aa1f229c4d26b818a9b
Mark Canlas <github@htmlism.com>
2016-08-31 13:13:15 -0400

    use full, top import

--- solver/src/test/scala/com/htmlism/spawningpool/SolverSpec.scala
+++ solver/src/test/scala/com/htmlism/spawningpool/SolverSpec.scala
@@ -2,10 +2,10 @@ package com.htmlism.spawningpool
 
 import org.specs2.mutable.Specification
 
+import com.htmlism.spawningpool.Solver._
+
 class SolverSpec extends Specification {
   "The solver" should {
-    import Solver._
-
     "select individuals at random" in {
       val rig = new RandomIndexProvider {
         val iterator = Iterable(0).iterator
