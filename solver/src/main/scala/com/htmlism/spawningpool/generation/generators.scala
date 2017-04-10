5fa99706432aee9b8eefb9e176849b262c254330
Mark Canlas <github@htmlism.com>
2017-04-10 19:11:06 -0400

    avoided class and objects in a package object

---
+++ solver/src/main/scala/com/htmlism/spawningpool/generation/generators.scala
@@ -0,0 +1,37 @@
+package com.htmlism.spawningpool
+package generation
+
+import scala.util.Random
+
+object IntGenerator extends Generator[Int] {
+  def generate: Int = Random.nextInt
+}
+
+object DoubleGenerator extends Generator[Double] {
+  def generate: Double = Random.nextDouble
+}
+
+object FixedIntArrayGenerator extends FixedIntArrayGenerator(DEFAULT_ARRAY_LENGTH, rngInt)
+object FixedDoubleArrayGenerator extends FixedDoubleArrayGenerator(DEFAULT_ARRAY_LENGTH, rngDouble)
+object VariableIntArrayGenerator extends VariableIntArrayGenerator(DEFAULT_ARRAY_LENGTH, rngInt, rngLength)
+object VariableDoubleArrayGenerator extends VariableDoubleArrayGenerator(DEFAULT_ARRAY_LENGTH, rngDouble, rngLength)
+
+// fixed
+
+class FixedIntArrayGenerator(length: Int, nextGene: () => Int) extends Generator[Array[Int]] {
+  def generate: Array[Int] = Array.fill(length)(nextGene.apply)
+}
+
+class FixedDoubleArrayGenerator(length: Int, nextGene: () => Double) extends Generator[Array[Double]] {
+  def generate: Array[Double] = Array.fill(length)(nextGene.apply)
+}
+
+// variable
+
+class VariableIntArrayGenerator(max: Int, nextGene: () => Int, nextLength: Int => Int) extends Generator[Array[Int]] {
+  def generate: Array[Int] = Array.fill(nextLength(max))(nextGene.apply)
+}
+
+class VariableDoubleArrayGenerator(max: Int, nextGene: () => Double, nextLength: Int => Int) extends Generator[Array[Double]] {
+  def generate: Array[Double] = Array.fill(nextLength(max))(nextGene.apply)
+}
--- solver/src/main/scala/com/htmlism/spawningpool/generation/package.scala
+++ solver/src/main/scala/com/htmlism/spawningpool/generation/package.scala
@@ -3,43 +3,10 @@ package com.htmlism.spawningpool
 import scala.util.Random
 
 package object generation {
-  private val DEFAULT_ARRAY_LENGTH = 100
+  private[generation] val DEFAULT_ARRAY_LENGTH = 100
 
-  private val rngInt    = () => Random.nextInt
-  private val rngDouble = () => Random.nextDouble
+  private[generation] val rngInt    = () => Random.nextInt
+  private[generation] val rngDouble = () => Random.nextDouble
 
-  private val rngLength = (max: Int) => Random.nextInt(max)
-
-  object IntGenerator extends Generator[Int] {
-    def generate: Int = Random.nextInt
-  }
-
-  object DoubleGenerator extends Generator[Double] {
-    def generate: Double = Random.nextDouble
-  }
-
-  object FixedIntArrayGenerator extends FixedIntArrayGenerator(DEFAULT_ARRAY_LENGTH, rngInt)
-  object FixedDoubleArrayGenerator extends FixedDoubleArrayGenerator(DEFAULT_ARRAY_LENGTH, rngDouble)
-  object VariableIntArrayGenerator extends VariableIntArrayGenerator(DEFAULT_ARRAY_LENGTH, rngInt, rngLength)
-  object VariableDoubleArrayGenerator extends VariableDoubleArrayGenerator(DEFAULT_ARRAY_LENGTH, rngDouble, rngLength)
-
-  // fixed
-
-  class FixedIntArrayGenerator(length: Int, nextGene: () => Int) extends Generator[Array[Int]] {
-    def generate: Array[Int] = Array.fill(length)(nextGene.apply)
-  }
-
-  class FixedDoubleArrayGenerator(length: Int, nextGene: () => Double) extends Generator[Array[Double]] {
-    def generate: Array[Double] = Array.fill(length)(nextGene.apply)
-  }
-
-  // variable
-
-  class VariableIntArrayGenerator(max: Int, nextGene: () => Int, nextLength: Int => Int) extends Generator[Array[Int]] {
-    def generate: Array[Int] = Array.fill(nextLength(max))(nextGene.apply)
-  }
-
-  class VariableDoubleArrayGenerator(max: Int, nextGene: () => Double, nextLength: Int => Int) extends Generator[Array[Double]] {
-    def generate: Array[Double] = Array.fill(nextLength(max))(nextGene.apply)
-  }
+  private[generation] val rngLength = (max: Int) => Random.nextInt(max)
 }
