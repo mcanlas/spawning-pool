be3247d87fe266a605e3e7be503da71ead4901ba
Mark Canlas <github@htmlism.com>
2017-10-25 00:52:55 -0400

    added ordinal fitness

---
+++ spawning-pool-core-alpha/src/main/scala/com/htmlism/spawningpool/fitness/OrdinalFitness.scala
@@ -0,0 +1,16 @@
+package com.htmlism.spawningpool.fitness
+
+/**
+  * @tparam A The element type whose fitness is being evaluated
+  */
+trait OrdinalFitness[A] {
+  def compare(x: A, y: A): Int
+}
+
+class FitnessByOrdering[A : Ordering] extends OrdinalFitness[A] {
+  def compare(x: A, y: A): Int = ???
+}
+
+class RatioFitness[A, B : Numeric](f: A => B) extends OrdinalFitness[A] {
+  def compare(x: A, y: A): Int = ???
+}
