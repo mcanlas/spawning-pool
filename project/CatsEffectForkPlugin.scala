5472fe17666faa8a82713ea0bdcb45e9617621c1
Mark Canlas <github@htmlism.com>
2023-09-29 11:10:38 -0400

    fork for cats :smiley_cat:

---
+++ project/CatsEffectForkPlugin.scala
@@ -0,0 +1,20 @@
+import sbt.*
+import sbt.Keys.*
+
+/**
+  * Automatically enriches projects with the following settings (despite the word "override").
+  */
+object CatsEffectForkPlugin extends AutoPlugin {
+
+  /**
+    * Thus plug-in will automatically be enabled; it has no requirements.
+    */
+  override def trigger: PluginTrigger =
+    AllRequirements
+
+  // cats-effect prefers to run in its own main thread
+  // https://github.com/typelevel/cats-effect/pull/3774
+  override val buildSettings: Seq[Setting[_]] = Seq(
+    fork := true
+  )
+}
