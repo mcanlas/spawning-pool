c4125e64e2d0decb9e817f300bbd9b0077f3fc11
Mark Canlas <github@htmlism.com>
2024-06-05 10:16:08 -0400

    cats 2.12.0

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -27,7 +27,7 @@ object ProjectPlugin extends AutoPlugin {
     implicit class ProjectOps(p: Project) {
       def withCats: Project =
         p
-          .settings(libraryDependencies += "org.typelevel" %% "cats-core" % "2.10.0")
+          .settings(libraryDependencies += "org.typelevel" %% "cats-core" % "2.12.0")
 
       def withEffectMonad: Project =
         p.settings(libraryDependencies += "org.typelevel" %% "cats-effect" % "3.5.4")
