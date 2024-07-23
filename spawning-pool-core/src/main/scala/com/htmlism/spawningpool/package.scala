ca14958b3fecec5abfc29997c146874380994431
Mark Canlas <github@htmlism.com>
2024-07-22 22:30:43 -0400

    drop memoization

--- spawning-pool-core/src/main/scala/com/htmlism/spawningpool/package.scala
+++ spawning-pool-core/src/main/scala/com/htmlism/spawningpool/package.scala
@@ -16,17 +16,6 @@ package object spawningpool {
 
   implicit def countToInt(count: PositiveCount): Int = count.count
 
-  def memoize[A, B](f: A => B): A => B = {
-    val cache = collection.mutable.Map[A, B]()
-
-    { key =>
-      if (!cache.contains(key))
-        cache(key) = f(key)
-
-      cache(key)
-    }
-  }
-
   implicit val asdf: Mutation[Int] = IntMutation
 
   implicit class MutationOps[A: Mutation](x: A) {
