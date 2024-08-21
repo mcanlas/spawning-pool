9e1a618b98a6246b3d84dc73d68bd7288f2dccc5
Mark Canlas <github@htmlism.com>
2024-08-21 11:28:39 -0400

    wrap array

--- spawning-pool-core/src/main/scala/com/htmlism/spawningpool/combinatorics/specialized/HomogenousCombinator.scala
+++ spawning-pool-core/src/main/scala/com/htmlism/spawningpool/combinatorics/specialized/HomogenousCombinator.scala
@@ -1,5 +1,6 @@
 package com.htmlism.spawningpool.combinatorics.specialized
 
+import scala.collection.immutable.ArraySeq
 import scala.reflect.ClassTag
 
 import com.htmlism.spawningpool.combinatorics
@@ -8,6 +9,6 @@ trait HomogenousCombinator[A] extends combinatorics.HomogenousCombinator[A] {
   implicit def classTag: ClassTag[A]
 
   override def fill(size: Int): (=> A) => Seq[A] = { fill =>
-    Array.fill[A](size)(fill)
+    ArraySeq.unsafeWrapArray(Array.fill[A](size)(fill))
   }
 }
