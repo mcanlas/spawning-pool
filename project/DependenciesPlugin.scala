efbcdc1992ca1e9e56fa606da7f69c4cf553dd81
Mark Canlas <github@htmlism.com>
2026-03-13 22:01:27 -0400

    cats-effect 3.7.0 :bubbles:

---
+++ project/DependenciesPlugin.scala
@@ -0,0 +1,36 @@
+import sbt.Keys.*
+import sbt.*
+
+object DependenciesPlugin extends AutoPlugin {
+  override def trigger = allRequirements
+
+  object autoImport {
+    implicit class DependencyOps(p: Project) {
+      def withCats: Project =
+        p
+          .settings(libraryDependencies += "org.typelevel" %% "cats-core" % "2.13.0")
+
+      def withEffectMonad: Project =
+        p.settings(libraryDependencies += "org.typelevel" %% "cats-effect" % "3.7.0")
+
+      def withTesting: Project = {
+        val weaverVersion =
+          "0.10.1"
+
+        p.settings(
+          libraryDependencies ++= Seq(
+            "org.typelevel" %% "weaver-cats"       % weaverVersion % Test,
+            "org.typelevel" %% "weaver-scalacheck" % weaverVersion % Test
+          )
+        )
+      }
+
+      def withYaml: Project =
+        p.settings(
+          libraryDependencies ++= Seq(
+            "io.circe" %% "circe-yaml" % "0.15.1"
+          )
+        )
+    }
+  }
+}
--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -23,33 +23,5 @@ object ProjectPlugin extends AutoPlugin {
     def module(s: String): Project =
       Project(s, file(jarName(s)))
         .settings(name := jarName(s))
-
-    implicit class ProjectOps(p: Project) {
-      def withCats: Project =
-        p
-          .settings(libraryDependencies += "org.typelevel" %% "cats-core" % "2.13.0")
-
-      def withEffectMonad: Project =
-        p.settings(libraryDependencies += "org.typelevel" %% "cats-effect" % "3.6.3")
-
-      def withTesting: Project = {
-        val weaverVersion =
-          "0.10.1"
-
-        p.settings(
-          libraryDependencies ++= Seq(
-            "org.typelevel" %% "weaver-cats"       % weaverVersion % Test,
-            "org.typelevel" %% "weaver-scalacheck" % weaverVersion % Test
-          )
-        )
-      }
-
-      def withYaml: Project =
-        p.settings(
-          libraryDependencies ++= Seq(
-            "io.circe" %% "circe-yaml" % "0.15.1"
-          )
-        )
-    }
   }
 }
