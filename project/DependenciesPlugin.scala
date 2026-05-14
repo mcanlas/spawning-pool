f534bec7b0eefd13f287779704a509b799d6f6c2
Mark Canlas <github@htmlism.com>
2026-05-14 01:28:12 -0400

    cats-core version :sun:

--- project/DependenciesPlugin.scala
+++ project/DependenciesPlugin.scala
@@ -11,7 +11,7 @@ object DependenciesPlugin extends AutoPlugin {
 
       def withCats: Project =
         p
-          .settings(libraryDependencies += "org.typelevel" %% "cats-core" % "2.13.0")
+          .settings(libraryDependencies += "org.typelevel" %% "cats-core" % Versions.catsCore)
 
       def withEffectMonad: Project =
         p.settings(libraryDependencies += "org.typelevel" %% "cats-effect" % Versions.catsEffect)
--- project/Versions.scala
+++ project/Versions.scala
@@ -1,4 +1,4 @@
 object Versions {
-  val catsEffect =
-    "3.7.0"
+  val catsCore   = "2.13.0"
+  val catsEffect = "3.7.0"
 }
